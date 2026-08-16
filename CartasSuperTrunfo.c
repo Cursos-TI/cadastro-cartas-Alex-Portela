61b322c5b9742129d5cc65114662980e4b44f9da
Alexandre Portela <74799797+Alex-Portela@users.noreply.github.com>
2026-08-16 00:35:57 +0000

    Adicionei comentário de várias linhas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,6 +3,57 @@
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+// Organização da cartas.
+/*
+Organização das Cartas refrentes ao Brasil
+ESTADO A — SÃO PAULO
+A01 - São Paulo
+A02 - Campinas
+A03 - Santos
+A04 - Ribeirão Preto
+
+ESTADO B — RIO DE JANEIRO
+B01 - Rio de Janeiro
+B02 - Niterói
+B03 - Petrópolis
+B04 - Angra dos Reis
+
+ESTADO C — MINAS GERAIS
+C01 - Belo Horizonte
+C02 - Uberlândia
+C03 - Ouro Preto
+C04 - Juiz de Fora
+
+ESTADO D — BAHIA
+D01 - Salvador
+D02 - Feira de Santana
+D03 - Porto Seguro
+D04 - Vitória da Conquista
+
+ESTADO E — PARANÁ
+E01 - Curitiba
+E02 - Londrina
+E03 - Foz do Iguaçu
+E04 - Maringá
+
+ESTADO F — PERNAMBUCO
+F01 - Recife
+F02 - Olinda
+F03 - Caruaru
+F04 - Petrolina
+
+ESTADO G — CEARÁ
+G01 - Fortaleza
+G02 - Juazeiro do Norte
+G03 - Sobral
+G04 - Caucaia
+
+ESTADO H — RIO GRANDE DO SUL
+H01 - Porto Alegre
+H02 - Caxias do Sul
+H03 - Gramado
+H04 - Pelotas
+*/
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
